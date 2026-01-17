fcd574c1aae837204952c90a131fc6e6599138ff
Kikoharuna <naufallgt@gmail.com>
2026-01-17 22:05:01 +0700

    ganti warna background biar cerah

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -106,7 +106,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F2F2F7",
+    backgroundColor: "#F8F9FA",
   },
   headerContainer: {
     padding: 20,
