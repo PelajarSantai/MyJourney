2287d255d367c3c5d74a3aa3f1de6e6839db2df5
PelajarSantai <muhammadraditia25@gmail.com>
2026-01-18 00:01:54 +0700

    tambah input judul

--- mobile/app/create.tsx
+++ mobile/app/create.tsx
@@ -1,10 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 export default function CreateLogScreen() {
+  const [title, setTitle] = useState('');
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Buat Catatan Baru 📝</Text>
+
+      <Text style={styles.label}>Judul Log</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Contoh: Perjalanan ke Bandung..."
+        value={title}
+        onChangeText={setTitle}
+      />
     </View>
   );
 }
@@ -21,5 +30,19 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginTop: 40,
     color: '#333',
+  },
+  label: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 12,
+    borderRadius: 8,
+    fontSize: 16,
+    marginBottom: 20,
+    backgroundColor: '#f9f9f9',
   },
 });
